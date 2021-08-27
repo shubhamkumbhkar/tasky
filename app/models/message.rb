7f09490827b758fd72ae2c2404ed7d532f36fde5
Shubham Kumbhkar <shkumbhkar@bestpeers.com>
2021-08-28 01:28:03 +0530

    Added bootstrap cdn

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,7 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
 
+  validates :body, presence: true
+
   after_create_commit { MessageBroadcastJob.perform_later(self) }
 end

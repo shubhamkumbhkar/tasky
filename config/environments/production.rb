634e7d9aa94121ec22635fc0bbca2d3f1aaff102
Shubham Kumbhkar <shkumbhkar@bestpeers.com>
2021-09-01 11:30:49 +0530

    bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  # config.action_cable.mount_path = ‘/cable’
-  config.action_cable.url = 'wss://intense-cove-68616.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://intense-cove-68616.herokuapp.com', /http:\/\/intense-cove-68616.herokuapp.com.*/ ]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,5 @@ Rails.application.routes.draw do
       resources :comments, module: :tasks
     end
   end
-  mount ActionCable.server => ‘/cable’
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
